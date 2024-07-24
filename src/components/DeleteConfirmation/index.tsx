265660d8af5e2637adf0216329a0b2e6d0ef9a3b
DhruvGahoi <dhruvgahoi11111@gmail.com>
2024-07-25 00:24:57 +0530

    added deleteConfirmation

--- src/components/DeleteConfirmation/index.tsx
+++ src/components/DeleteConfirmation/index.tsx
@@ -1,3 +1,47 @@
-const DeleteConfirmation : React.FC = () => {
-    const { taskToDelete, dispatch } = useStore()
-}
+import { ACTIONS } from '../../constants/AppConstants';
+import { useStore } from '../../contexts/Store';
+import { useTaskService } from '../../services/tasks';
+import ConfirmationDialog from '../ConfirmationDialog';
+
+const DeleteConfirmation: React.FC = () => {
+  const { taskToDelete, dispatch } = useStore();
+  const { deleteTask } = useTaskService();
+
+  function deleteTasks() {
+    if (!taskToDelete) return;
+    deleteTask(taskToDelete.id)
+      .then(() => {
+        dispatch({
+          type: ACTIONS.DELETE_TASK,
+          payload: {
+            taskId: taskToDelete.id,
+            completed: taskToDelete.completed,
+          },
+        });
+        dispatch({
+          type: ACTIONS.SET_TASK_TO_DELETE,
+          payload: { taskToDelete: null },
+        });
+      })
+      .catch((error) => console.error(error));
+  }
+
+  function handleClose() {
+    dispatch({
+      type: ACTIONS.SET_TASK_TO_DELETE,
+      payload: { taskToDelete: null },
+    });
+  }
+
+  return (
+    <ConfirmationDialog
+      open={true}
+      handleClose={handleClose}
+      handleConfirm={deleteTasks}
+      title="Delete task?"
+      subTitle="The tasks will be irreversibly deleted from the system"
+    />
+  );
+};
+
+export default DeleteConfirmation;
