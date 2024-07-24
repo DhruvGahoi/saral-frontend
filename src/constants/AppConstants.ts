19d4493b71ff9017d584ce4a3a01b5378d8a08a8
DhruvGahoi <dhruvgahoi11111@gmail.com>
2024-07-24 23:58:22 +0530

    added constants

---
+++ src/constants/AppConstants.ts
@@ -0,0 +1,8 @@
+export const ACTIONS = {
+    UPDATE_TASKS : 'update_tasks',
+    UPDATE_TASK : 'update_task',
+    ADD_TASK : 'add_task',
+    DELETE_TASK : 'delete_task',
+    SET_TASK_TO_UPDATE : 'set_task_to_update',
+    SET_TASK_TO_DELETE : 'set_task_to_delete' 
+}
